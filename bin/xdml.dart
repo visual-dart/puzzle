8868facdcd6ed32b2638896533f9179fb764c6b8
Big Mogician <sunaoran@youzan.com>
2019-08-21 14:54:27 +0800

    fix: watch mode

--- bin/xdml.dart
+++ bin/xdml.dart
@@ -1,13 +1,15 @@
 import "../lib/main.dart";
 import '../lib/utils.dart';
 
-// dart main.dart --entry=../../../../GIT/fe-assets/assets-flutter/example/lib --group=com.example.lib
+// dart xdml.dart --entry=../../../../GIT/fe-assets/assets-flutter/example --group=com.example --watch=true
 
 void main(List<String> arguments) {
   var argus = parseArguments(arguments);
   var target = argus.firstWhere((i) => i[0] == "entry", orElse: () => null);
   var target2 = argus.firstWhere((i) => i[0] == "group", orElse: () => null);
+  var target3 = argus.firstWhere((i) => i[0] == "watch", orElse: () => null);
   return parse(
       entry: target == null ? "." : target[1],
-      group: target2 == null ? "com.example" : target2[1]);
+      group: target2 == null ? "com.example" : target2[1],
+      watch: target3 == null ? false : target2[1] == "true");
 }
